cc09b8dcd64f23798aa7ec8e6daee7fc70f8a19c
Brian Takita <brian.takita@gmail.com>
2023-04-05 20:22:01 -0400

    ## 1.1.88
    ### Patch Changes
    
    - @ctx-core/nanostores: 3.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/solid-nanostores
 
+## 1.1.88
+
+### Patch Changes
+
+- @ctx-core/nanostores: 3.2.0
+
 ## 1.1.87
 
 ### Patch Changes
--- src/useReadable/index.js
+++ src/useReadable/index.js
@@ -1,7 +1,7 @@
-import { split_atom_ } from '@ctx-core/nanostores'
+import { atom_pair_ } from '@ctx-core/nanostores'
 import { observable } from 'solid-js'
 export function useReadable(accessor) {
-	const [computed_, set] = split_atom_()
+	const [computed_, set] = atom_pair_()
 	observable(accessor).subscribe($=>set($))
 	return computed_
 }
